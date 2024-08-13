d5e07774a6a2fa60f3c9b02014da9b10a98d6d12
krithik10 <153708683+krithik10@users.noreply.github.com>
2024-08-13 10:37:07 +0530

    Delete BACKEND/demo/src/main/java/com/example/demo/repository directory

--- BACKEND/demo/src/main/java/com/example/demo/repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.ContactModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<ContactModel, Long> {
-}
--- BACKEND/demo/src/main/java/com/example/demo/repository/loginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.login;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface loginRepository extends JpaRepository<login, Long> {
-    Optional<login> findByUsername(String username);
-}
